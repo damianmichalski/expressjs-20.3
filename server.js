214797442760310a19a6b9cd97f123e012f950ea
DamianM <damian.michalski@kramp.com>
2019-02-08 13:54:16 +0100

    Zmiana updateNote

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var app = express();
+var fs= require('fs');
+
 var stringifyFile;
 
 app.use(bodyParser.json());
 
-var server = app.listen(3000);
-
-app.get('/', function(req, res) {
-  console.log('Otrzymałem żądanie GET do strony głównej');
-  res.send('Hello GET!');
-});
-
-app.get('/getNote', function(req, res) {
+app.get('/getNote', function(req, res){
   fs.readFile('./test.json', 'utf8', function(err, data) {
     if (err) throw err;
-    stringifyFile = data
+    stringifyFile = data;
     res.send(data);
   });
 });
 
-app.post('/updateNote/:note', function (req, res) {
-  stringifyFile += req.params.note;
+app.post('/updateNote/:note', function(req, res) {
   fs.writeFile('./test.json', stringifyFile, function(err) {
     if (err) throw err;
-    console.log('file updated');
+    stringifyFile = req.params.note;
+    res.send(stringifyFile);
+    console.log('file updated: ' + stringifyFile);
   });
-  res.send('file updated');
 });
 
-app.use(function (req, res, next) {
-  res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+var server = app.listen(3000);
