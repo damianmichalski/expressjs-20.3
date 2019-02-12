fa3937b9184b185a4da897b8133433cfc1696270
DamianM <damian.michalski@kramp.com>
2019-02-12 10:47:36 +0100

    Poprawa updateNote

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var fs= require('fs');
+var fs = require('fs');
 
-var stringifyFile;
+const jsonFile = './test.json';
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function(req, res){
-  fs.readFile('./test.json', 'utf8', function(err, data) {
+app.get('/getNote', function (req, res) {
+  fs.readFile(jsonFile, 'utf8', function (err, data) {
     if (err) throw err;
-    stringifyFile = data;
     res.send(data);
   });
 });
 
 app.post('/updateNote/:note', function (req, res) {
-  if (stringifyFile === 'undefined') {
-    res.redirect('/getNote');
-  }
-  stringifyFile = req.params.note;
-  fs.writeFile('./test.json', stringifyFile, function(err) {
+  fs.readFile(jsonFile, 'utf8', function (err, data) {
     if (err) throw err;
-    console.log('file updated');
-    res.send('File updated: ' + stringifyFile);
+    const dataObject = JSON.parse(data);
+
+    const { note } = req.params;
+    dataObject.notes.push(note);
+    data = JSON.stringify(dataObject);
+
+    fs.writeFile(jsonFile, data, function (err) {
+      if (err) throw err;
+      console.log('file updated');
+      res.send(data);
+    });
   });
 });
 
